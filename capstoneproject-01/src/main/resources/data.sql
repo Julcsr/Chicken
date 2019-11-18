c6be52d4b1d37219b5d50ed31bdeba97841a5b8d
Rusian-eitor <jeff11_22@hotmail.com>
2019-11-18 18:27:12 -0500

    Test.

--- capstoneproject-01/src/main/resources/data.sql
+++ capstoneproject-01/src/main/resources/data.sql
@@ -32,7 +32,7 @@ INSERT INTO offices(id,phone_number,adress,email,boss) VALUES
 
 
 
-INSERT INTO  users(enabled,password,user_name) VALUES(true,'$2a$10$sr2ZuTYlZ.LsliI6sv.HqeH39WsA.gz9Fdr.S0CyCBByyPQ8TObvS','chicken001');
+INSERT INTO  users(enabled,password,user_name) VALUES(true,'$2a$10$sr2ZuTYlZ.LsliI6sv.HqeH39WsA.gz9Fdr.S0CyCBByyPQ8TObvS','chicken001'); 
 INSERT INTO  users(enabled,password,user_name) VALUES(true,'$2a$10$ZoqMrCUHNN8lE2g1zssyuucNhExlYwN06kM/Jo6vEBhyTQduQ7iCy','usercode');
 
 
